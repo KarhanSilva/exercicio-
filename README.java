1cef289ffbc28d683602565d167d3a8df6789e50
KarhanSilva <karhandasilva@outlook.com>
2015-09-25 14:59:38 -0300

    Create README.java
    O c eu acho que ta totalmente errado, mais não achei ele la na lista do professor, como que multiplico só uma linha ?

---
+++ README.java
@@ -0,0 +1,47 @@
+# exercicio-
+.
+exercicio matrizes 
+
+ import javax.swing.*;
+public class ExerciciosMatrizes
+public static void main (String[]args){
+
+int numero,matriz;
+numero= Integer.parseInt(JOptionPane.showInputDialog("Digite numero para a matriz");
+int matriz[][] = new int [3][3]
+}
+}
+
+a) //Calcule e mostre a soma dos elementos da primeira coluna
+
+int i=0;
+int j=0;
+
+for(i=0,i<3,i++)for(j=0,j<3,j++) 
+JOptionPane.showMessageDialog(null"elementosPrimeiraColuna");
+}
+}
+
+b)// Calcule e mostre a soma dos elementos de cada coluna
+
+
+
+
+
+int i=0;
+for(i=0,i<3,i++)
+
+
+
+
+
+
+
+c)//Calcule e mostre o produto dos elementos da primeira linha
+
+
+
+int i=1
+int j=1
+
+for(i=1,i<4,i++){for (int j=1,j<4,j++)if (produto==numeros*i)JOptionPane.showMessageDialog{null,"aqui estão os elementos da primeira linha"}}}}}
